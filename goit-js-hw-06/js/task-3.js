6cca6e616e9011b7e8ef489bfb0306389bb94944
Marik713 <62308344+Marik713@users.noreply.github.com>
2020-09-23 18:54:10 +0300

    second attempt

--- goit-js-hw-06/js/task-3.js
+++
@@ -1,5 +0,0 @@
-import users from '/users.js'
-
-const getUsersWithGender = (users, gender) => users.filter(user => (user.gender === gender)).map(user => user.name);
-
-console.log(getUsersWithGender(users, 'male'));
